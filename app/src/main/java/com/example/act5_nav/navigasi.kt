6f62210ff98497b37f095a50d6e88d8606f7cd30
aTaruna <aTaruna@users.noreply.github.com>
2025-11-06 12:32:15 +0700

    membuat function di file navigasi

---
+++ app/src/main/java/com/example/act5_nav/navigasi.kt
@@ -0,0 +1,43 @@
+package com.example.act5_nav
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+enum class Navigasi{
+    FormulirQue,
+    Detail
+}
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier
+){
+    Scaffold { isiRuang ->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.FormulirQue.name,
+            modifier = modifier.padding(isiRuang)) {
+            composable(route = Navigasi.FormulirQue.name) {
+                FormIsian(
+                    OnSubmitBtnClick = {
+                        navController.navigate(Navigasi.Detail.name)
+                    }
+                )
+            }
+            composable(route = Navigasi.Detail.name) {
+                TampilData(
+                    OnBackBtnClick = {
+                        cancelAndBackToFormulirQue(navController)
+                    }
+                )
+            }
+        }
+    }
+}
