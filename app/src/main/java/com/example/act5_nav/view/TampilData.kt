fbc0a0eb0d697406f4e42107e0890cbaa4e0f74d
aTaruna <aTaruna@users.noreply.github.com>
2025-11-06 14:45:53 +0700

    menambah button pada file TampilData.kt

--- app/src/main/java/com/example/act5_nav/view/TampilData.kt
+++ app/src/main/java/com/example/act5_nav/view/TampilData.kt
@@ -2,8 +2,13 @@ package com.example.act5_nav.view
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -15,7 +20,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.act5_nav.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -49,8 +57,18 @@ fun TampilData(
                         Column {
                             Text(text = item.first.uppercase(),
                                 fontSize = 16.sp)
-                            Text(text = item.second)
+                            Text(text = item.second,
+                                fontWeight = FontWeight.Bold,
+                                fontFamily = FontFamily.Cursive,
+                                fontSize = 22.sp)
                         }
+                        HorizontalDivider(thickness = 1.dp, color = Color.Cyan)
+                    }
+                    Spacer(modifier = Modifier.height(10.dp))
+                    Button(
+                        modifier = Modifier.fillMaxWidth(),
+                        onClick = onBackBtnClick) {
+                        Text(text = stringResource(id = R.string.back))
                     }
                 }
             }
