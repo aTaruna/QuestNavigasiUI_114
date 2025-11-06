9f94efafe504063139fd7801b3b70bf6b14858c8
aTaruna <aTaruna@users.noreply.github.com>
2025-11-06 13:51:48 +0700

    menambah scaffold

--- app/src/main/java/com/example/act5_nav/view/TampilData.kt
+++ app/src/main/java/com/example/act5_nav/view/TampilData.kt
@@ -1,2 +1,32 @@
 package com.example.act5_nav.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import com.example.act5_nav.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TampilData(
+    onBackBtnClick:()->Unit
+){
+    val items = listOf(
+        Pair(stringResource(id = R.string.nama_lengkap), "Contoh Nama"),
+        Pair(stringResource(id = R.string.jenke), "Lainnya"),
+        Pair(stringResource(id = R.string.alamat), "Jogja")
+    )
+    Scaffold (modifier = Modifier,
+        {
+            TopAppBar(
+                title = { Text(stringResource(id = R.string.tampil))},
+                colors = TopAppBarDefaults.topAppBarColors
+                    (containerColor = colorResource(id = R.color.teal_700))
+            )
+        }){}
+}
