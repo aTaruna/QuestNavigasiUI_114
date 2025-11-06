5419c07cf55839960d65b90ea76d3c4fe4ef2616
aTaruna <aTaruna@users.noreply.github.com>
2025-11-06 13:24:14 +0700

    menambah scaffold pada file formulir

--- app/src/main/java/com/example/act5_nav/view/Formulir.kt
+++ app/src/main/java/com/example/act5_nav/view/Formulir.kt
@@ -1,10 +1,19 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
 package com.example.act5_nav.view
 
-import android.R
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import com.example.act5_nav.R
+
 
 @Composable
 fun FormIsian(
@@ -14,7 +23,11 @@ fun FormIsian(
     Scaffold (modifier = Modifier,
         {
             TopAppBar(
-
-            )
-        }
+                title = { Text(stringResource(id = R.string.home),
+                    color = Color.White)},
+                colors = TopAppBarDefaults.topAppBarColors
+                    (containerColor = colorResource(id = R.color.teal_700))
+            )})
+    {isiRuang ->
+    }
 }
