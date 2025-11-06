6c9d55de75d1224dabdce7f1d2ec648274a944e2
aTaruna <aTaruna@users.noreply.github.com>
2025-11-06 14:04:41 +0700

    menambah layout column pada isiRuang

--- app/src/main/java/com/example/act5_nav/view/TampilData.kt
+++ app/src/main/java/com/example/act5_nav/view/TampilData.kt
@@ -1,14 +1,21 @@
 package com.example.act5_nav.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.act5_nav.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -24,9 +31,28 @@ fun TampilData(
     Scaffold (modifier = Modifier,
         {
             TopAppBar(
-                title = { Text(stringResource(id = R.string.tampil))},
-                colors = TopAppBarDefaults.topAppBarColors
+                title = {
+                    Text(stringResource(id = R.string.tampil),
+                        color = Color.White)},
+                colors = TopAppBarDefaults.mediumTopAppBarColors
                     (containerColor = colorResource(id = R.color.teal_700))
             )
-        }){}
+        }){isiRuang ->
+            Column (modifier = Modifier.padding(isiRuang),
+                verticalArrangement = Arrangement.SpaceBetween){
+                Column (
+                    modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
+                    verticalArrangement = Arrangement
+                        .spacedBy(dimensionResource(id = R.dimen.padding_small))
+                ){
+                    items.forEach { item->
+                        Column {
+                            Text(text = item.first.uppercase(),
+                                fontSize = 16.sp)
+                            Text(text = item.second)
+                        }
+                    }
+                }
+            }
+        }
 }
