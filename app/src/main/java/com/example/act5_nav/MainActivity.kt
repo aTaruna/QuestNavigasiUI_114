dc023af8b50d2384bf2169a697af10af1fc0b6b1
aTaruna <aTaruna@users.noreply.github.com>
2025-11-06 14:06:32 +0700

    mengubah isi mainactivity

--- app/src/main/java/com/example/act5_nav/MainActivity.kt
+++ app/src/main/java/com/example/act5_nav/MainActivity.kt
@@ -19,29 +19,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Act5_navTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    DataApp(modifier = Modifier)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Act5_navTheme {
-        Greeting("Android")
     }
 }
